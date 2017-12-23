cdd5e5934d062c0410a30c5dcd78356d579a74e3
Ekrem Çetinkaya <ekrem.cetinkaya@ozu.edu.tr>
2017-12-23 12:39:56 +0300

    Processed Depth values and implemented action taking model based on depth values

--- main.py
+++ main.py
@@ -7,15 +7,27 @@ if __name__ == '__main__':
     env.reset_base()
 
     destination_point = env.destination
+    #  Act randomly
     while destination_point == env.destination:
         random_action = randint(0, 2)
         env.act(random_action)
-
+        print(env.depth_image_raw)
         print("---")
         print("Random Action", random_action)
         print("Position", env.position)
         print("Destination", env.destination)
         print("---")
 
-    print("Destination reached!")
+    """
+    #  Act according to depth values
+    random_action = randint(0, 2)
+    env.act(0)
+    while destination_point == env.destination:
+        depth = env.depth_image_raw
+        action = env.decide_action_based_on_depth(depth)
+        env.act(action)
+        print(action)
+        print("-------------------------------------------")
+    """
 
+    print("Destination reached!")

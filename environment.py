e09f9799f095f2ff75139b50b39555e1e59279d7
erdenbatuhan <erden.batuhan@gmail.com>
2018-05-11 12:02:47 -0400

    merge two goals

--- environment.py
+++ environment.py
@@ -5,17 +5,19 @@ import cv2
 import numpy as np
 import image_preprocessor as ipp
 
+import message_filters
 from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.msg import ModelState, ModelStates
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
+from kobuki_msgs.msg import BumperEvent
 from cv_bridge import CvBridge, CvBridgeError
 
 
 class Environment:
 
     ROSPY_FREQUENCY = 10
-    NUM_OF_SUBSCRIPTIONS = 1
+    MAX_CRASH_COUNT = 10
 
     def __init__(self, base_name):
         rospy.init_node("Environment", anonymous=False)
@@ -24,18 +26,30 @@ class Environment:
 
         self.vel_pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=5)
         self.rate = rospy.Rate(self.ROSPY_FREQUENCY)
-        self.bridge = CvBridge()
         self.initial_time = time.time()
         self.base_name = base_name
+        
+        # Create the cv_bridge objects
+        self.rgb_bridge = CvBridge()
+        self.depth_bridge = CvBridge()
 
-        self.depth_image_raw = None
+        self.image = None
+        self.depth_image = None
+
+        self.has_green = False
         self.terminal = False
         self.crashed = False
 
-        self.subscriptions_ready = np.zeros(self.NUM_OF_SUBSCRIPTIONS)
-        self.subscribe_depth_image_raw()
+        self.crash_counter = 0
+        self.history = np.zeros(self.MAX_CRASH_COUNT)
 
-        self.wait_for_subscriptions()
+        # self.subscribe()
+
+        self.subscribe_rgb_image_raw()
+        # self.subscribe_depth_image_raw()
+        self.subscribe_bumper_event()
+
+        self.wait_for_image()
 
     def shutdown(self):
         rospy.loginfo("TurtleBot is stopping..")
@@ -44,36 +58,95 @@ class Environment:
         rospy.sleep(1)
         rospy.loginfo("TurtleBot stopped!")
 
-    def wait_for_subscriptions(self):
-        while np.sum(self.subscriptions_ready) < self.NUM_OF_SUBSCRIPTIONS:
+    def wait_for_image(self):
+        while self.image is None and self.depth_image is None:
             pass
 
-    def depth_image_raw_callback(self, depth_image_raw):
+    def callback(self, image_raw, depth_image_raw):
+    	_image, _depth_image = None, None
+    	self.image, self.depth_image = None, None
+
         try:
-            self.depth_image_raw = self.bridge.imgmsg_to_cv2(depth_image_raw, "32FC1")
+            _image = self.rgb_bridge.imgmsg_to_cv2(image_raw, "bgr8")
+            _depth_image = self.depth_bridge.imgmsg_to_cv2(depth_image_raw, "32FC1")
+
+            if np.average(_image) > 1.:
+            	self.image = np.array(_image, dtype=np.uint8)
+
+            if np.average(_depth_image) < 1.:
+            	self.depth_image = np.array(_depth_image, dtype=np.float32) / 255.0
         except CvBridgeError as e:
             print(e)
 
-        self.depth_image_raw = np.array(self.depth_image_raw, dtype=np.float32)
-        self.subscriptions_ready[0] = 1
+    def subscribe(self):
+    	image_raw = message_filters.Subscriber("/camera/rgb/image_raw", Image)
+        depth_image_raw = message_filters.Subscriber("/camera/depth/image_raw", Image)
+
+        ts = message_filters.TimeSynchronizer([image_raw, depth_image_raw], 100)
+        ts.registerCallback(self.callback)
+
+    def rgb_image_raw_callback(self, rgb_image_raw):
+        self.image = None
+
+        try:
+            self.image = self.rgb_bridge.imgmsg_to_cv2(rgb_image_raw, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        self.image = np.array(self.image, dtype=np.uint8)
+
+    def subscribe_rgb_image_raw(self):
+        rospy.Subscriber("/camera/rgb/image_raw", Image, self.rgb_image_raw_callback)
+
+    def depth_image_raw_callback(self, depth_image_raw):
+        self.depth_image = None
+
+        try:
+            self.depth_image = self.depth_bridge.imgmsg_to_cv2(depth_image_raw, "32FC1")
+        except CvBridgeError as e:
+            print(e)
+
+        self.depth_image = np.array(self.depth_image, dtype=np.float32)
+        self.depth_image = self.depth_image / 255.0
 
     def subscribe_depth_image_raw(self):
         rospy.Subscriber("/camera/depth/image_raw", Image, self.depth_image_raw_callback)
 
-    def get_state(self):
-        # Check if crashed
-        depth = ipp.preprocess_image(self.depth_image_raw)
-        
-        if np.average(np.array(depth[0][0])) <= 0.05 / 255:
-            self.crashed = True
+    def bumper_event_callback(self, bumper_event):
+        self.crashed = True if bumper_event.bumper == 1 else False
 
-        return depth
+    def subscribe_bumper_event(self):
+        rospy.Subscriber("/mobile_base/events/bumper", BumperEvent, self.bumper_event_callback)
+
+    def get_state(self):
+        try:
+            curr_image = self.image
+            
+            frame = ipp.preprocess_image(curr_image)
+            mask, self.has_green, self.terminal = ipp.get_greens(curr_image)
+
+            if self.crash_counter < self.MAX_CRASH_COUNT:
+                self.history[self.crash_counter] = np.average(frame)
+                self.crash_counter = self.crash_counter + 1
+            else:
+                self.crash_counter = 0
+                if -0.01 <= np.average(self.history) - np.average(frame) <= 0.01:
+                    self.crashed = True
+            
+            state = np.array([np.array([frame, mask])], dtype=np.uint8)
+            return state
+        except cv2.error as e:
+            return np.zeros((1, 2, 80, 80), dtype=uint8)
 
     def get_reward(self):
-        if self.crashed:
+    	if self.terminal:
+    		return 100
+        elif self.crashed:
             return -1
+        elif self.has_green:
+        	return 0.1
 
-        return 0.1
+        return 0
 
     def act(self, action, v1=0.3, v2=0.05):
         vel_cmd = Twist()
@@ -82,13 +155,13 @@ class Environment:
             vel_cmd.linear.x = v1 - v2
             vel_cmd.angular.z = 2. * v1
         elif action == 1:  # FORWARD - LEFT
-            vel_cmd.linear.x = v1 - v2
+            vel_cmd.linear.x = 1.5 * (v1 - v2)
             vel_cmd.angular.z = v1
         elif action == 2:  # FORWARD - AHEAD
-            vel_cmd.linear.x = 2. * (v1 - v2)
+            vel_cmd.linear.x = 2.5 * (v1 - v2)
             vel_cmd.angular.z = 0.
         elif action == 3:  # FORWARD - RIGHT
-            vel_cmd.linear.x = v1 - v2
+            vel_cmd.linear.x = 1.5 * (v1 - v2)
             vel_cmd.angular.z = -v1
         elif action == 4:  # RIGHT
             vel_cmd.linear.x = v1 - v2
@@ -100,13 +173,14 @@ class Environment:
         self.vel_pub.publish(vel_cmd)
         self.rate.sleep()
 
-        self.subscriptions_ready = np.zeros(self.NUM_OF_SUBSCRIPTIONS)
-        self.wait_for_subscriptions()
+        self.image = None
+        self.wait_for_image()
 
         state = self.get_state()
         reward = self.get_reward()
 
-        print("State {} | Reward {} | Act {}".format(state, reward, (action - 2)))
+        print("Has Green? {} | Terminal {} | Crashed {} | Reward {} | Act {}".
+        	format(self.has_green, self.terminal, self.crashed, reward, (action - 2)))
         return state, reward, self.terminal, self.crashed
 
     @staticmethod
@@ -137,11 +211,12 @@ class Environment:
 
         self.shutdown()
 
+        self.has_green = False
         self.terminal = False
         self.crashed = False
 
-        self.subscriptions_ready = np.zeros(self.NUM_OF_SUBSCRIPTIONS)
-        self.wait_for_subscriptions()
+        self.image = None
+        self.wait_for_image()
 
         self.initial_time = time.time()
 
--- image_preprocessor.py
+++ image_preprocessor.py
@@ -8,21 +8,29 @@ import numpy as np
 
 
 def preprocess_image(img):
+	# sConvert the image to grayscale
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 	# Delete the very inner information from the image
 	img = np.reshape(img, (img.shape[0], img.shape[1]))
 
 	# Resize the image
 	img = cv2.resize(img, (80, 80))
 
-	for i in range(0, img.shape[0]):
-		for j in range(0, img.shape[1]):
-			if np.isnan(img[i][j]):
-				img[i][j] = -1
-
-	# Reshape the image
-	img = np.array([np.array([img])])
-
-	# Normalize
-	img = img / 255.0
-
 	return img
+
+def get_greens(img):
+	try:
+		hsv = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2HSV)
+		mask = cv2.inRange(src=hsv, lowerb=(29, 86, 6), upperb=(64, 255, 255))
+
+		_mask = cv2.resize(mask, (80, 80))
+	except cv2.error as e:
+		return np.zeros((80, 80)), False, False
+
+	if cv2.countNonZero(mask) <= 50:
+		return np.zeros((80, 80)), False, False
+	elif cv2.countNonZero(mask) > mask.shape[0] * mask.shape[1] * .60:
+		return _mask, True, True
+
+	return _mask, True, False
